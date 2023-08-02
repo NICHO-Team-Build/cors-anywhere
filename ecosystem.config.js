2543d3c6e3d995ccfb6e0824a758d29a7de8cf7f
Lance <reminisences@163.com>
2023-08-02 10:47:50 +0800

    rename app name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       cwd: "./",
-      name: "nichonft-proxy-ai",
+      name: "cors-anywhere",
       script: "server.js",
       watch: false,
       instances: 1,
-      error_file: "/data/logs/nichonft-proxy-ai/error.log",
-      out_file: "/data/logs/nichonft-proxy-ai/server.log",
-      pid_file: "/data/logs/nichonft-proxy-ai/server.pid",
+      error_file: "/data/logs/cors-anywhere/error.log",
+      out_file: "/data/logs/cors-anywhere/server.log",
+      pid_file: "/data/logs/cors-anywhere/server.pid",
     },
   ],
 }
